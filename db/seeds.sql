e20d781d1c68f6198181883416e474b37a30e2a7
Ricardo <manosalvas2013@gmail.com>
2020-08-15 19:54:13 -0400

    for grading

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Spicy Chicken Deluxe');
+INSERT INTO burgers (name, devoured) VALUES ('Bacanator', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name, devoured) VALUES ('McDouble', true);
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
